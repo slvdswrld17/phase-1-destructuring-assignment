c4a62ea59f5094bcb88102e31b2149622339baa0
Salehe <salehe.mkomwa@student.moringaschool.com>
2025-04-01 10:02:13 +0300

    destructing

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
+// Strings
 const farmAnimals = 'cow horse sheep pig chicken';
 
+// 1. Assign variables based on the sounds animals make
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+console.log(moo, neigh, baa, oink, cluck); // cow horse sheep pig chicken
+
+// 2. Assign four animals, skipping the horse
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
+console.log(bessie, dolly, babe, little); // cow sheep pig chicken
+
+// 3. Assign three animals with color-related names, skipping horse and chicken
+const [blackAndWhite, , black, pink] = farmAnimals.split(' ');
+console.log(blackAndWhite, black, pink); // cow sheep pig
+
+// Arrays
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
+// 4. Assign color variables
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+console.log(red, orange, yellow, green, blue, indigo, violet);
+
+// 5. Assign colors using first letters, skipping indigo
+const [r, o, y, g, b, , v] = colors;
+console.log(r, o, y, g, b, v);
+
+// 6. Assign indigo only
+const [, , , , , indg] = colors;
+console.log(indg);
+
+// Objects
 const muppet = {
   muppetName: 'Miss Piggy',
   color: 'pink',
@@ -9,6 +36,10 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+
+// 7. Destructure all variables from muppet
+const { muppetName, color, song, job, partner } = muppet;
+console.log(muppetName, color, song, job, partner);
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -25,24 +56,11 @@ const nestedMuppet = {
   nestedPartner: 'Miss Piggy'
 };
 
-// Strings
+// 8. Destructure songs 2 and 4, and Kermit's job and partner
+const { 
+  album: { theMuppetMovie: { song2, song4 } }, 
+  nestedJob, 
+  nestedPartner 
+} = nestedMuppet;
 
-// 1. Use destructuring to assign appropriate variables based on the sounds animals make.
-
-// 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
-
-// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
-
-// Arrays
-
-// 4. Use destructuring to assign appropriate variables using the color names.
-
-// 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
-
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
-
-// Objects
-
-// 7. Use destructuring to assign all variables using the keys as the variable names
-
-// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+console.log(song2, song4, nestedJob, nestedPartner);
